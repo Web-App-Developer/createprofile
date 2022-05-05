dbdf628a698c13b778f4fcc34fa95d2fcf40c7c6
Web-App-Developer <lionking921117@gmail.com>
2022-05-05 04:46:00 -0400

    Crendential Section Complete

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,8 @@ $(document).ready(function () {
         Add_Skills_Section(pass_skillcount);
     });
     $("#w-node-fd5f3992-2c7f-76a2-4117-70a223359dc0-25be96d7").on('click', function () {
-        Add_Credential();
+        var CrendentialCount = getCredentialCounters();
+        Add_Credential(CrendentialCount);
     });
 
 
@@ -15,6 +16,14 @@ $(document).ready(function () {
         // /////////////////////////////////
         var skillCountAfterRemove = getQuizCounters();
         Rerender_Skills_Section(skillCountAfterRemove);
+     });
+
+    $('#webflow_credentials').on('click', 'a.delete', function(events){
+        /////////// Remove_Skills_Section()
+        $(this).parents('div').parents('div').eq(1).remove();
+        // /////////////////////////////////
+        var CredentialCounters = getCredentialCounters();
+        Rerender_Crendentials_Section(CredentialCounters);
      });
 });
 
@@ -76,30 +85,48 @@ function Rerender_Skills_Section(renderCount) {
 }
 
 
-function Add_Credential() {
-    var Credentials = $("#webflow_credentials >  .quiz-buiilder-question");
-    var CredentialsCount = Credentials.length;
-    var i = parseInt(CredentialsCount);
-    
-    var addCredentialSection = `
-            <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db5-25be96d7" class="w-layout-grid profile-add-row"> 
-                <div id = "w-node-fd5f3992-2c7f-76a2-4117-70a223359db6-25be96d7" class="profile-add-row-column" > 
-                    <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db7-25be96d7">`+ (k +2) +`</div>
-                </div > 
-                <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db9-25be96d7" class="quiz-builder-question"> 
-                    <label for="Education-Credential" class="modal-input-label-copy">Credential</label> 
-                    <input type="text" class="modal-input w-input" maxlength="256" name="Education-Credential" data-name="Education Credential" placeholder="" id="Education-Credential"> 
-                        <label for="Education-Issuer" class="modal-input-label">Issuer</label> 
-                        <input type="text" class="modal-input w-input" maxlength="256" name="Education-Issuer" data-name="Education Issuer" placeholder="" id="Education-Issuer"> 
-                        <a href="#" class="button dashboard-body modal-button delete quiz w-button"> 
-                            <span class="inline-leading-icon"> 
-                            <strong></strong> 
-                        </span>+Remove Credential</a> 
-                </div> 
-            </div>`;
-            $("#webflow_credentials").append(addCredentialSection);
-    }
+function Add_Credential(CrendentialCount) {
+    var addCredentialSection = '';
+    addCredentialSection = `
+    <div class="w-layout-grid profile-add crendentials">
+        <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db5-25be96d7" class="w-layout-grid profile-add-row">
+        <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db6-25be96d7" class="profile-add-row-column">
+            <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db7-25be96d7">`+ (CrendentialCount + 2) +`</div>
+        </div>
+        <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db9-25be96d7" class="quiz-builder-question"><label for="Education-Credential" class="modal-input-label-copy">Credential</label><input type="text" class="modal-input w-input" maxlength="256" name="Education-Credential" data-name="Education Credential" placeholder="" id="Education-Credential"><label for="Education-Issuer" class="modal-input-label">Issuer</label><input type="text" class="modal-input w-input" maxlength="256" name="Education-Issuer" data-name="Education Issuer" placeholder="" id="Education-Issuer">
+            <a href="#" class="button dashboard-body modal-button delete quiz w-button"><span class="inline-leading-icon"><strong></strong></span>Remove Credential</a>
+        </div>
+        </div>
+    </div>`;
+     $("#webflow_credentials").append(addCredentialSection);
+}
 
-function Remove_Skills_Section() {
-    $(this).parents('div').parents('div').eq(1).remove();
+function getCredentialCounters(){
+    const Credentials = $("#webflow_credentials > .crendentials");
+    const CredentialsCount = parseInt(Credentials.length);
+    return CredentialsCount;
+}
+
+function Rerender_Crendentials_Section(CredentialCounters){
+    var appendFile = ''
+    var addCredentialSection = '';
+    const crendentials = document.querySelectorAll('.crendentials');
+    crendentials.forEach(crendential => {
+        crendential.remove();
+    });
+    for (var k = 0 ; k < CredentialCounters; k++){
+        addCredentialSection += `
+            <div class="w-layout-grid profile-add crendentials">
+                <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db5-25be96d7" class="w-layout-grid profile-add-row">
+                <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db6-25be96d7" class="profile-add-row-column">
+                    <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db7-25be96d7">`+ (k + 2) +`</div>
+                </div>
+                <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db9-25be96d7" class="quiz-builder-question"><label for="Education-Credential" class="modal-input-label-copy">Credential</label><input type="text" class="modal-input w-input" maxlength="256" name="Education-Credential" data-name="Education Credential" placeholder="" id="Education-Credential"><label for="Education-Issuer" class="modal-input-label">Issuer</label><input type="text" class="modal-input w-input" maxlength="256" name="Education-Issuer" data-name="Education Issuer" placeholder="" id="Education-Issuer">
+                    <a href="#" class="button dashboard-body modal-button delete quiz w-button"><span class="inline-leading-icon"><strong></strong></span>Remove Credential</a>
+                </div>
+                </div>
+            </div>`;
+    }
+    appendFile += addCredentialSection;
+    $("#webflow_credentials").append(appendFile);
 }
