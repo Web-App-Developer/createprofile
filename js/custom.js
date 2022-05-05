4a503304b4440c5721922abca8802178dc3d9677
Web-App-Developer <lionking921117@gmail.com>
2022-05-05 04:01:21 -0400

    Add_Quiz_Part complete

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
     $("#w-node-_0941d12c-eae9-33b6-8979-5d22a2baec9c-25be96d7").on('click', function () {
-
         var pass_skillcount = getQuizCounters();
         Add_Skills_Section(pass_skillcount);
     });
@@ -19,21 +18,20 @@ $(document).ready(function () {
 
 
 function getQuizCounters(){
-    var skills = $("#webflow_skills > .profile-add");
+    var skills = $("#webflow_skills > .profile-add-new");
     var skillsCount = skills.length;
-    var pass_skillcount = parseInt(skillsCount) + 1;
+    var pass_skillcount = parseInt(skillsCount);
     console.log("=======skillCount====", pass_skillcount)
     return pass_skillcount
 }
 
 
 function Add_Skills_Section(skillCount) {
-    console.log("SkillsCount", skillCount)
     var addSkillSection = `
-        <div class="w-layout-grid profile-add">
+        <div class="w-layout-grid profile-add profile-add-new">
             <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb93d-25be96d7" class="w-layout-grid quiz-buiilder-question">
                 <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb93e-25be96d7" class="quiz-builder-question-number">
-                    <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb93f-25be96d7">`+ skillCount +`</div>
+                    <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb93f-25be96d7">`+ (skillCount + 2)+`</div>
                 </div>
                 <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb941-25be96d7" class="quiz-builder-question">
                     <input type="text" class="modal-input w-input" maxlength="256" name="Skill" data-name="Skill" placeholder="" id="Skill">
@@ -48,29 +46,32 @@ function Add_Skills_Section(skillCount) {
 }
 
 
-function Rerender_Skills_Section(skillCount) {
-    console.log("Rerender_Skills_Section", skillCount)
-    // var appendFile = ''
-    // $(".profile-add").empty();
-    // appendFile += `<h3 class="heading-19">Skills</h3>`; 
-    // appendFile += `<p class="paragraph-5">You may add up to 8 skills.</p>`; 
-    // var addSkillSection = `
-    //     <div class="w-layout-grid profile-add">
-    //         <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb93d-25be96d7" class="w-layout-grid quiz-buiilder-question">
-    //             <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb93e-25be96d7" class="quiz-builder-question-number">
-    //                 <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb93f-25be96d7">`+ skillCount +`</div>
-    //             </div>
-    //             <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb941-25be96d7" class="quiz-builder-question">
-    //                 <input type="text" class="modal-input w-input" maxlength="256" name="Skill" data-name="Skill" placeholder="" id="Skill">
-    //                 <a href="#" class="button dashboard-body modal-button delete quiz w-button">
-    //                     <span class="inline-leading-icon"><strong></strong></span>Remove Skill
-    //                 </a>
-    //             </div>
-    //         </div>
-    //     </div>
-    // `;
-    // appendFile += addSkillSection;
-    // $("#webflow_skills").append(appendFile);
+function Rerender_Skills_Section(renderCount) {
+    var appendFile = ''
+    var addSkillSection = '';
+    const quizs = document.querySelectorAll('.profile-add-new');
+    quizs.forEach(quiz => {
+        quiz.remove();
+    });
+    for (var k = 0 ; k < renderCount; k++){
+        addSkillSection += `
+            <div class="w-layout-grid profile-add profile-add-new">
+                <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb93d-25be96d7" class="w-layout-grid quiz-buiilder-question">
+                    <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb93e-25be96d7" class="quiz-builder-question-number">
+                        <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb93f-25be96d7">`+ (k + 2) +`</div>
+                    </div>
+                    <div id="w-node-d37d29a3-5d3b-a003-ff40-6e0b9eefb941-25be96d7" class="quiz-builder-question">
+                        <input type="text" class="modal-input w-input" maxlength="256" name="Skill" data-name="Skill" placeholder="" id="Skill">
+                        <a href="#" class="button dashboard-body modal-button delete quiz w-button">
+                            <span class="inline-leading-icon"><strong></strong></span>Remove Skill
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+    appendFile += addSkillSection;
+    $("#webflow_skills").append(appendFile);
 }
 
 
@@ -79,25 +80,25 @@ function Add_Credential() {
     var CredentialsCount = Credentials.length;
     var i = parseInt(CredentialsCount);
     // console.log("CredentialsCount", i)
-    var addCredentialSection =`
-        <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db5-25be96d7" class="w-layout-grid profile-add-row"> 
-            <div id = "w-node-fd5f3992-2c7f-76a2-4117-70a223359db6-25be96d7" class="profile-add-row-column" > 
-                <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db7-25be96d7">1</div>
-            </div > 
-            <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db9-25be96d7" class="quiz-builder-question"> 
-                <label for="Education-Credential" class="modal-input-label-copy">Credential</label> 
-                <input type="text" class="modal-input w-input" maxlength="256" name="Education-Credential" data-name="Education Credential" placeholder="" id="Education-Credential"> 
-                    <label for="Education-Issuer" class="modal-input-label">Issuer</label> 
-                    <input type="text" class="modal-input w-input" maxlength="256" name="Education-Issuer" data-name="Education Issuer" placeholder="" id="Education-Issuer"> 
-                    <a href="#" class="button dashboard-body modal-button delete quiz w-button"> 
-                        <span class="inline-leading-icon"> 
-                        <strong></strong> 
-                    </span>+Remove Credential</a> 
-            </div> 
-        </div>`;
-    $("#webflow_credentials").append(addCredentialSection);
-
-}
+    
+    var addCredentialSection = `
+            <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db5-25be96d7" class="w-layout-grid profile-add-row"> 
+                <div id = "w-node-fd5f3992-2c7f-76a2-4117-70a223359db6-25be96d7" class="profile-add-row-column" > 
+                    <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db7-25be96d7">`+ (k +2) +`</div>
+                </div > 
+                <div id="w-node-fd5f3992-2c7f-76a2-4117-70a223359db9-25be96d7" class="quiz-builder-question"> 
+                    <label for="Education-Credential" class="modal-input-label-copy">Credential</label> 
+                    <input type="text" class="modal-input w-input" maxlength="256" name="Education-Credential" data-name="Education Credential" placeholder="" id="Education-Credential"> 
+                        <label for="Education-Issuer" class="modal-input-label">Issuer</label> 
+                        <input type="text" class="modal-input w-input" maxlength="256" name="Education-Issuer" data-name="Education Issuer" placeholder="" id="Education-Issuer"> 
+                        <a href="#" class="button dashboard-body modal-button delete quiz w-button"> 
+                            <span class="inline-leading-icon"> 
+                            <strong></strong> 
+                        </span>+Remove Credential</a> 
+                </div> 
+            </div>`;
+            $("#webflow_credentials").append(addCredentialSection);
+    }
 
 function Remove_Skills_Section() {
     alert('REMOVEACTION');
