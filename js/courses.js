4691eb54b95bd0e99cf82f3fc6331d81b3dc412c
Web-App-Developer <lionking921117@gmail.com>
2022-05-06 16:28:36 -0400

    5/7 When page loads and when questions are appended, they should use the expanded state.

--- js/courses.js
+++ js/courses.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
 
     // Section Expended function
     $('.grid-builder-questions').on('click', 'a.quiz', function(e){
-        $(".grid-builder-questions").find(".expended").css("display", "none");
-        $(".grid-builder-questions").find(".collapse").css("display", "block");
+        // $(".grid-builder-questions").find(".expended").css("display", "none");
+        // $(".grid-builder-questions").find(".collapse").css("display", "block");
         $(this).parents('.collapse').next('.expended').css("display", "block");
         $(this).parents('.collapse').css("display", "none");
     });
@@ -16,8 +16,8 @@ $(document).ready(function () {
 
     // Section Collapsed function
     $('.grid-builder-questions').on('click','a.minimize', function (e) {
-        $(".grid-builder-questions").find(".expended").css("display", "none");
-        $(".grid-builder-questions").find(".collapse").css("display", "block");
+        // $(".grid-builder-questions").find(".expended").css("display", "none");
+        // $(".grid-builder-questions").find(".collapse").css("display", "block");
         $(this).parents('.expended').prev('.collapse').css("display", "block");
         $(this).parents('.expended').css("display", "none");
     });
@@ -50,7 +50,7 @@ function getQuizsCount(){
 function addQuizs(quizsCount){
     var appendFile = '';
     const k= quizsCount;
-    appendFile += `<div id="w-node-_86f4356f-b9e5-12e3-8c89-35891eba50bb-bbbe96d4" class="w-layout-grid quiz-builder-question-wrap collapse">
+    appendFile += `<div id="w-node-_86f4356f-b9e5-12e3-8c89-35891eba50bb-bbbe96d4" class="w-layout-grid quiz-builder-question-wrap collapse"  style="display:none;">
             <div id="w-node-_58ade7b9-d8a5-4d37-1e12-a57e681ea8b9-bbbe96d4" class="quiz-builder-question-number">
                         <div id="w-node-_121a5b3b-c8da-277a-bf3d-8cbd696ba5e3-bbbe96d4">Q`+ (k + 1) +`</div>
                         </div>
@@ -77,7 +77,7 @@ function addQuizs(quizsCount){
                         <a id="w-node-_155079ec-3884-ea4d-8d0a-2387b9881bdb-bbbe96d4" href="#" class="button dashboard-body modal-button quiz w-button"><span class="inline-leading-icon"></span>Expand Question</a>
                     </div>
 
-                    <div id="w-node-_8db722d7-adae-2408-ef7c-7eefc014bb53-bbbe96d4" class="w-layout-grid quiz-builder-question-wrap expended" style="display:none;">
+                    <div id="w-node-_8db722d7-adae-2408-ef7c-7eefc014bb53-bbbe96d4" class="w-layout-grid quiz-builder-question-wrap expended">
                             <div id="w-node-ef986d02-1c34-986b-3f96-b916195a7846-bbbe96d4" class="quiz-builder-question-number">
                             <div id="w-node-ef986d02-1c34-986b-3f96-b916195a7847-bbbe96d4">Q`+ (k + 1) +`</div>
                             </div>
@@ -116,7 +116,7 @@ function renderAccordian(quizsCount){
     });
     for (var k = 0 ; k < quizsCount; k++){
         appendFile += 
-        `<div id="w-node-_86f4356f-b9e5-12e3-8c89-35891eba50bb-bbbe96d4" class="w-layout-grid quiz-builder-question-wrap collapse">
+        `<div id="w-node-_86f4356f-b9e5-12e3-8c89-35891eba50bb-bbbe96d4" class="w-layout-grid quiz-builder-question-wrap collapse"  style="display:none;">
             <div id="w-node-_58ade7b9-d8a5-4d37-1e12-a57e681ea8b9-bbbe96d4" class="quiz-builder-question-number">
             <div id="w-node-_121a5b3b-c8da-277a-bf3d-8cbd696ba5e3-bbbe96d4">Q`+ (k + 1) +`</div>
             </div>
@@ -143,7 +143,7 @@ function renderAccordian(quizsCount){
             <a id="w-node-_155079ec-3884-ea4d-8d0a-2387b9881bdb-bbbe96d4" href="#" class="button dashboard-body modal-button quiz w-button"><span class="inline-leading-icon"></span>Expand Question</a>
         </div>
 
-        <div id="w-node-_8db722d7-adae-2408-ef7c-7eefc014bb53-bbbe96d4" class="w-layout-grid quiz-builder-question-wrap expended" style="display:none;">
+        <div id="w-node-_8db722d7-adae-2408-ef7c-7eefc014bb53-bbbe96d4" class="w-layout-grid quiz-builder-question-wrap expended">
                 <div id="w-node-ef986d02-1c34-986b-3f96-b916195a7846-bbbe96d4" class="quiz-builder-question-number">
                     <div id="w-node-ef986d02-1c34-986b-3f96-b916195a7847-bbbe96d4">Q`+ (k + 1) +`</div>
                     </div>
